c7ecc8f20968626a3b464e53b9ece0c36c41bd02
Pranav Chandra Paruchuri <53783667+pranavchandra-dotcom@users.noreply.github.com>
2024-05-03 00:14:44 -0700

    Update LinkedListTest.cpp
    Added DeepState_Int() to the testing that generates the random input.

--- LinkedListTest.cpp
+++ LinkedListTest.cpp
@@ -8,34 +8,38 @@ TEST(LinkedList, TestAllScenarios) {
 
     // Test insertAtBeginning
     for (int val = 1; val <= 10; ++val) {
-        list.insertAtBeginning(val);
+        int value = DeepState_IntInRange(1, 10);
+        list.insertAtBeginning(value);
         CHECK(list.head != nullptr);
-        CHECK(list.head->data == val);
+        CHECK(list.head->data == value);
     }
     cout<<"Done test1"<<endl;
     // Test insertAtEnd
     for (int val = 11; val <= 20; ++val) {
-        list.insertAtEnd(val);
+         int value = DeepState_IntInRange(11, 20);
+        list.insertAtEnd(value);
         CHECK(list.tail != nullptr);
-        CHECK(list.tail->data == val);
+        CHECK(list.tail->data == value);
     }
         cout<<"Done test2"<<endl;
 
     // Test insertAtPosition
     for (int val = 21; val <= 30; ++val) {
+        int value = DeepState_IntInRange(21, 30);
         int previous_size = list_size();
         for (int position = 0; position <= previous_size; ++position) {
-            list.insertAtPosition(val, position);
+            list.insertAtPosition(value, position);
             Node* current = list.get(position)
-            CHECK(current->data == val);
+            CHECK(current->data == value);
         }
     }
     cout<<"Done test3"<<endl;
 
     // Test deleteAtBeginning
     for (int val = 1; val <= 10; ++val) {
+        Node* previous = list.head;
         list.deleteAtBeginning();
-        CHECK(list.head == nullptr || list.head->data != val);
+        CHECK(list.head == nullptr || list.head->data != previous->data);
     }
     cout<<"Done test4"<<endl;
 
